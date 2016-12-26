1299457f10a139fa820910d6758b87d5e152d7d1
Arina I <ariiigu@gmail.com>
2016-12-25 20:11:01 -0500

    Finished 2nd sprint: case studies.

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -25,19 +25,44 @@ get_header(); ?>
 	</div><!-- .container -->
 </section><!-- .home-page -->
 
-<section class="recent-posts">
-	<div class="site-content">
-		<div class="blog-post">
-			<h4>From the Blog</h4>
-			<?php query_posts('posts_per_page=1'); ?>
-				<?php while ( have_posts() ) : the_post(); ?>
-					<h2><?php the_title(); ?></h2>
-					<?php the_excerpt(); ?> 
-					<a class="read-more-link" href="<?php the_permalink(); ?>">Read More <span>&rsaquo;</span></a>
-				<?php endwhile; ?> 
-			<?php wp_reset_query(); ?>
+<div class="featured-content-container">
+	<section class="featured-work">
+		<div class="site-content">
+			<h4>FEATURED WORK</h4>
+			<ul class="homepage-featured-work">
+				<?php query_posts('posts_per_page=3&post_type=case_studies'); ?>
+					<?php while ( have_posts() ) : the_post(); 
+						$image_1 = get_field("image_1");
+						$size = "medium";
+					?>
+						<li class="individual-featured-work">
+							<div class="individual-featured-work-container">
+								<figure>
+									<?php echo wp_get_attachment_image($image_1, $size); ?>
+								</figure>
+								<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+							</div>
+						</li>
+					<?php endwhile; ?> 
+				<?php wp_reset_query(); ?>
+			</ul>
 		</div>
-	</div>
-</section>
+	</section>
+
+	<section class="recent-posts">
+		<div class="site-content">
+			<div class="blog-post">
+				<h4>From the Blog</h4>
+				<?php query_posts('posts_per_page=1'); ?>
+					<?php while ( have_posts() ) : the_post(); ?>
+						<h2><?php the_title(); ?></h2>
+						<?php the_excerpt(); ?> 
+						<a class="read-more-link" href="<?php the_permalink(); ?>">Read More <span>&rsaquo;</span></a>
+					<?php endwhile; ?> 
+				<?php wp_reset_query(); ?>
+			</div>
+		</div>
+	</section>
+</div>
 
 <?php get_footer(); ?>
